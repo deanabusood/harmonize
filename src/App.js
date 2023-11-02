10a639fbd3d4b5627699659bb2dc2cb534a5062a
dean <75696492+deanabusood@users.noreply.github.com>
2023-11-01 21:32:17 -0400

    Add searchbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import SearchBar from "./components/SearchBar";
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SearchBar></SearchBar>
+    </div>
+  );
 };
 
 export default App;
