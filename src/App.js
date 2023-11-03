a13aab79b12af2f10b6efb5f5ee4b1841c59c991
dean <75696492+deanabusood@users.noreply.github.com>
2023-11-02 20:17:52 -0400

    Create communication between searchbar and resultsdisplay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
+import ResultsDisplay from "./components/ResultsDisplay";
 
-const App = () => {
+function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [genreIds, setGenreIds] = useState([]);
+
+  const handleSearch = (results, genres) => {
+    setSearchResults(results);
+    setGenreIds(genres);
+  };
+
+  const handleMovieClick = (index) => {
+    const selectedGenres = genreIds[index];
+    console.log("Genres:", selectedGenres);
+  };
+
   return (
-    <div className="App">
-      <SearchBar></SearchBar>
+    <div className="app-container">
+      <SearchBar onSearch={handleSearch} />
+      {searchResults.length > 0 && (
+        <ResultsDisplay
+          searchResults={searchResults}
+          genreIds={genreIds}
+          onMovieClick={handleMovieClick}
+        />
+      )}
     </div>
   );
-};
+}
 
 export default App;
