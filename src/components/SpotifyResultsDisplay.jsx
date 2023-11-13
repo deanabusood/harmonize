030419bbc1da7e83c7f26168ea1479f6fb6e0c6a
dean <75696492+deanabusood@users.noreply.github.com>
2023-11-12 19:50:43 -0500

    Add redirect to spotify song on image click

--- src/components/SpotifyResultsDisplay.jsx
+++ src/components/SpotifyResultsDisplay.jsx
@@ -9,14 +9,20 @@ function SpotifyResultsDisplay({ searchResults, handleGenerateClick }) {
     <ul className="results-list">
       {searchResults.tracks.map((result, index) => (
         <li key={result.id} className="result-item">
-          <img
-            src={
-              result.album.images.length > 0
-                ? result.album.images[0].url
-                : imageNotFound
-            }
-            alt={result.name}
-          />
+          <a
+            href={result.external_urls.spotify}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={
+                result.album.images.length > 0
+                  ? result.album.images[0].url
+                  : imageNotFound
+              }
+              alt={result.name}
+            />
+          </a>
           <div className="result-details">
             <p>{result.name}</p>
             <p>{result.artists.map((artist) => artist.name).join(", ")}</p>
